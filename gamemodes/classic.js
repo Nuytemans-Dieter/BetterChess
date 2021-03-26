c97387201a40083fec4575dc1541d991f723dddb
Dieter Nuytemans <dieter.nuytemans@student.uantwerpen.be>
2021-03-26 09:11:57 +0100

    Cleanup script

--- gamemodes/classic.js
+++ gamemodes/classic.js
@@ -146,43 +146,19 @@ class Game {
         // Copy the players in this game to the board
         board.players = this.players;
 
-        // Setup the pieces
-        let rows = [0, 1, 6, 7];
-        for (let i in rows)
-        for (let x=0; x<8; x++)
-        {
-            let y = rows[i];
+        // Hard coded, see dynamically generated solution at https://github.com/Nuytemans-Dieter/Nuytemans-Dieter.github.io/blob/46c32e4f550000085fb3196cdb286f2acd462ccf/gamemodes/classic.js#L143
+        let newBoard = [
+            ["white/rook", "white/knight", "white/bishop", "white/queen", "white/king", "white/bishop", "white/knight", "white/rook"],
+            ["white/pawn", "white/pawn", "white/pawn", "white/pawn", "white/pawn", "white/pawn", "white/pawn", "white/pawn"],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", ""],
+            ["black/pawn", "black/pawn", "black/pawn", "black/pawn", "black/pawn", "black/pawn", "black/pawn", "black/pawn"],
+            ["black/rook", "black/knight", "black/bishop", "black/queen", "black/king", "black/bishop", "black/knight", "black/rook"],
+        ];
 
-            let piece = "";
-            if (y == 1 || y == 6)
-                piece = "pawn";
-            else {
-                switch(x)
-                {
-                    case 0:
-                    case 7:
-                        piece = "rook";
-                        break;
-                    case 1:
-                    case 6:
-                        piece = "knight";
-                        break;
-                    case 2:
-                    case 5:
-                        piece = "bishop";
-                        break;
-                    case 3:
-                        piece = "queen";
-                        break;
-                    case 4:
-                        piece = "king";
-                        break;
-                }
-            }
-
-            let playerID = (y < 5) ? 1 : 0;
-            board.setPiece(x, y, piece, playerID);
-        }
+        board.setBoard(newBoard);
     };
 
     getLegalMoves(pieceName, pieceLocation, board) {
